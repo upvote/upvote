37ad2f14ccac66976d6df9bf2c7d1f0a495ace50
Chris Bierlinski <chris@shadow.io>
2015-02-11 22:10:25 -0800

    commit missing db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,26 +13,26 @@
 
 ActiveRecord::Schema.define(version: 20150206061412) do
 
-  create_table "authorizations", force: true do |t|
-    t.integer  "user_id",    null: false
-    t.string   "provider",   null: false
-    t.string   "uid",        null: false
+  create_table "authorizations", force: :cascade do |t|
+    t.integer  "user_id",                null: false
+    t.string   "provider",   limit: 255, null: false
+    t.string   "uid",        limit: 255, null: false
     t.text     "meta"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "handle"
+    t.string   "handle",     limit: 255
   end
 
   add_index "authorizations", ["provider", "uid"], name: "index_authorizations_on_provider_and_uid", unique: true
   add_index "authorizations", ["provider", "user_id"], name: "index_authorizations_on_provider_and_user_id", unique: true
 
-  create_table "comments", force: true do |t|
-    t.integer  "commentable_id",   default: 0
-    t.string   "commentable_type"
-    t.string   "title"
+  create_table "comments", force: :cascade do |t|
+    t.integer  "commentable_id",               default: 0
+    t.string   "commentable_type", limit: 255
+    t.string   "title",            limit: 255
     t.text     "body"
-    t.string   "subject"
-    t.integer  "user_id",          default: 0, null: false
+    t.string   "subject",          limit: 255
+    t.integer  "user_id",                      default: 0, null: false
     t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20150206061412) do
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
-  create_table "post_clicks", force: true do |t|
+  create_table "post_clicks", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "post_id",    null: false
     t.datetime "created_at"
@@ -53,27 +53,27 @@ ActiveRecord::Schema.define(version: 20150206061412) do
   add_index "post_clicks", ["post_id"], name: "index_post_clicks_on_post_id"
   add_index "post_clicks", ["user_id"], name: "index_post_clicks_on_user_id"
 
-  create_table "posts", force: true do |t|
-    t.string   "title",                               null: false
-    t.integer  "user_id",                             null: false
-    t.string   "type",         default: "Post::Base", null: false
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",        limit: 255,                        null: false
+    t.integer  "user_id",                                         null: false
+    t.string   "type",         limit: 255, default: "Post::Base", null: false
     t.text     "description"
-    t.string   "url"
+    t.string   "url",          limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
-    t.integer  "clicks_count", default: 0,            null: false
+    t.string   "slug",         limit: 255
+    t.integer  "clicks_count",             default: 0,            null: false
   end
 
   add_index "posts", ["clicks_count"], name: "index_posts_on_clicks_count"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "taggings", force: true do |t|
+  create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
-    t.string   "taggable_type"
+    t.string   "taggable_type", limit: 255
     t.integer  "tagger_id"
-    t.string   "tagger_type"
+    t.string   "tagger_type",   limit: 255
     t.string   "context",       limit: 128
     t.datetime "created_at"
   end
@@ -81,38 +81,38 @@ ActiveRecord::Schema.define(version: 20150206061412) do
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
-  create_table "tags", force: true do |t|
-    t.string  "name"
-    t.integer "taggings_count", default: 0
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count",             default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
-  create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.integer  "sign_in_count",      default: 0,  null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",              limit: 255, default: "", null: false
+    t.integer  "sign_in_count",                  default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip", limit: 255
+    t.string   "last_sign_in_ip",    limit: 255
     t.text     "meta"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "headline",           default: "", null: false
-    t.string   "name",               default: "", null: false
-    t.string   "slug"
-    t.string   "avatar"
+    t.string   "headline",           limit: 255, default: "", null: false
+    t.string   "name",               limit: 255, default: "", null: false
+    t.string   "slug",               limit: 255
+    t.string   "avatar",             limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
 
-  create_table "votes", force: true do |t|
+  create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
-    t.string   "votable_type"
+    t.string   "votable_type", limit: 255
     t.integer  "voter_id"
-    t.string   "voter_type"
+    t.string   "voter_type",   limit: 255
     t.boolean  "vote_flag"
-    t.string   "vote_scope"
+    t.string   "vote_scope",   limit: 255
     t.integer  "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
