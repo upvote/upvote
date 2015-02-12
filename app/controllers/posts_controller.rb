eb4313ef355ca93a0e68d4a0ff6098cd96b27ae7
Chris Bierlinski <chris@shadow.io>
2015-02-11 22:18:45 -0800

    remove cruft in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,15 +45,10 @@ class PostsController < ApplicationController
     @post = Post::Base.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
-
   # POST /posts
   # POST /posts.json
   def create
     @post = Post::Base.new post_params.merge(user:current_user)
-
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_comments_path(@post), notice: 'Post was successfully created.' }
@@ -65,31 +60,12 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_comments_path(@post), notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: post_comments_path(@post) }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
+
+    def handle_post_error(format,action=:edit)
+      format.html { render action }
+      format.json { render json: @post.errors, status: :unprocessable_entity }
+    end
 
     def set_user
       @user = User.friendly.find(params[:user_id])
