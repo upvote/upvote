2f960a1a418bb810d12f865de9f0c708aa3db9de
Chris Bierlinski <chris@shadow.io>
2015-02-11 22:28:31 -0800

    ensure URL is a valid URL for link submissions

--- app/models/post/link.rb
+++ app/models/post/link.rb
@@ -1,5 +1,14 @@
 module Post
   class Link < Post::Base
     validates :url, presence: true
+    validate :url_is_valid
+
+    def url_is_valid
+      uri = URI.parse(url)
+      raise 'invalid URL' unless ( uri.protocol == 'https' || uri.protocol == 'http' )
+    rescue
+      errors.add :url, 'is invalid'
+    end
+
   end
 end
